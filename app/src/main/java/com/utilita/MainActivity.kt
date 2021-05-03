23ec1bebd74ac28510dd5d6ed241acfa02b65969
Philip <aliasphilip@gmail.com>
2021-05-03 11:22:45 +0100

    Initial Commit

---
+++ app/src/main/java/com/utilita/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.utilita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.utilita.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
